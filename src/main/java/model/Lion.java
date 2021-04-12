752e50f419b341e9baf7e1652fc78cd4d36522e0
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-12 14:19:41 +0100

    Feat: Lion implemented.

---
+++ src/main/java/model/Lion.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.List;
+
+import static model.AnimalType.*;
+
+public class Lion extends Piece {
+
+    public Lion(Color color) {
+        super(color, LION, ELEPHANT);
+    }
+
+    public Lion(Lion lion){
+        super(lion.getColor(), lion.getAnimalType(), lion.getFear());
+    }
+    public List<Position> getLegalPositions(Board board) {
+        return null;
+    }
+
+    public Lion clone(){
+        return new Lion(this);
+    }
+}
