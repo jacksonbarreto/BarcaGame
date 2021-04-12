2205d77c8b2689bd9e6c46f01ef1566dcd9e2a99
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-12 13:13:32 +0100

    Feat: location implemented.

---
+++ src/main/java/model/Location.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.Objects;
+
+public class Location {
+    private final int row;
+    private final int column;
+
+    public Location(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return row == location.row && column == location.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+}
