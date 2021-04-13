1b91264151e6f8c7298777390573cb29fdbcc5fc
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-13 14:30:59 +0100

    Feat: struct for javafx.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,5 @@ module BarcaGame {
 
     opens  view to javafx.fxml;
     exports view;
+    exports model;
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -12,8 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 700));
         primaryStage.show();
+
     }
 
 
