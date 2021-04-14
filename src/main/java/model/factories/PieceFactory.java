f91ce0e5f7b7a72292f643a18db4df17383731de
Jackson Barreto <jackson.barreto@gmail.com>
2021-04-14 14:28:35 +0100

    Refactor: included location on the part.

--- src/main/java/model/factories/PieceFactory.java
+++ src/main/java/model/factories/PieceFactory.java
@@ -5,16 +5,16 @@ import model.*;
 public class PieceFactory {
 
 
-    public static Piece createLion(Color color) {
-        return new Lion(color);
+    public static Piece createLion(Color color, Location location) {
+        return new Lion(color, location);
     }
 
-    public static Piece createElephant(Color color) {
-        return new Elephant(color);
+    public static Piece createElephant(Color color, Location location) {
+        return new Elephant(color, location);
     }
 
-    public static Piece createMouse(Color color) {
-        return new Mouse(color);
+    public static Piece createMouse(Color color, Location location) {
+        return new Mouse(color, location);
     }
 
 }
